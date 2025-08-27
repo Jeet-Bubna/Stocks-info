e9a0bed0641b21208b2c6492b7be1fe7154800ea
Jeet Bubna <bubnajeet@gmail.com>
2025-08-27 13:09:19 +0530

    Switched to Zerodah

--- pythonfiles/tests/tests.py
+++ pythonfiles/tests/tests.py
@@ -5,14 +5,38 @@ from pythonfiles.main import get_stock_websites, scrape_data
 def test_get_stock_websites():
     stock_names = ['RELIANCE', 'TCS', 'INFY']
     expected_output = {
-        'RELIANCE': 'https://www.nseindia.com/get-quotes/equity?symbol=RELIANCE',
-        'TCS': 'https://www.nseindia.com/get-quotes/equity?symbol=TCS',
-        'INFY': 'https://www.nseindia.com/get-quotes/equity?symbol=INFY'
+        'RELIANCE': 'https://zerodha.com/markets/stocks/NSE/RELIANCE',
+        'TCS': 'https://zerodha.com/markets/stocks/NSE/TCS',
+        'INFY': 'https://zerodha.com/markets/stocks/NSE/INFY'
     }
     assert get_stock_websites(stock_names) == expected_output
 
-def test_scrape_data(url: str):
-    expcted_output = '3,054.70'  # Replace with the actual expected price for RELIANCE
-    assert scrape_data(url) == expcted_output
+def scrape_data_test():
+    url = 'https://zerodha.com/markets/stocks/NSE/TCS'
+    stock_name = 'TCS'
+    data = scrape_data(url, stock_name)
+    assert isinstance(data, dict)  # Check if the returned data is a dictionary
+    assert 'PE' in data  # Check if 'PE' key is in the returned dictionary
+    assert 'Sector PE' in data  # Check if 'Sector PE' key is in the returned dictionary
+    assert 'PB Ratio' in data  # Check if 'PB Ratio' key is in the returned dictionary
+    assert 'Div Yield' in data  # Check if 'Div Yield' key is in the returned dictionary
+    assert 'ROE' in data  # Check if 'ROE' key is in the returned dictionary
+    assert 'EBIDATA' in data  # Check if 'EBIDATA' key is in the returned dictionary
+    assert 'ROCE' in data  # Check if 'ROCE' key is in the returned dictionary
+    assert 'EPS' in data  # Check if 'EPS' key is in the returned dictionary
 
-test_scrape_data('https://www.nseindia.com/get-quotes/equity?symbol=TCS')
+
+def test_all():
+    try:
+        test_get_stock_websites()
+        print(f"test_get_stock..... ✅")
+    except AssertionError as e:
+        print(f"test_get_stock..... ❌: {e}")
+    
+    try:
+        scrape_data_test()
+        print(f"scrape_data........ ✅")
+    except AssertionError as e:
+        print(f"scrape_data........ ❌: {e}")
+    
+    print('All tests conducted')
